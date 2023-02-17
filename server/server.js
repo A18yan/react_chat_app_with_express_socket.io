a5715209276485a923580e340e66035e004a2808
A18yan <ma18yan@gmail.com>
2023-02-17 19:41:29 +0545

    socket server setup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require("express");
-
-const app = express();
 const mongoose = require("mongoose");
-// express parser 
+require("dotenv").config();
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+// express parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+// server -> http
+const server = http.createServer(app);
+// cors setup -> allows react app to communicate with server
+app.use(cors());
+// socket io -> also setups cors
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+// mongoose connection
 mongoose.connect(
   "mongodb://localhost/test",
   () => {
@@ -20,4 +34,6 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.listen(3000);
+server.listen(process.env.PORT, () => {
+  console.log(`Server running at port ${process.env.PORT}`);
+});
